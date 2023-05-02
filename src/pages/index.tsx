862bc64cb7e24ba175476a1e764350b6294d9d4d
Leo Goubard <leogoubard@macbook-pro-de-leo.home>
2023-05-02 19:18:08 +0200

    :construction: wip

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,12 +1,31 @@
-/* eslint-disable @next/next/no-img-element */
-import React, { useEffect, useState, Suspense } from 'react';
+import React from 'react';
+import Spline from '@splinetool/react-spline';
 
 
 const Home = () => {
 
   return (
-    <div>
-      HELLO THERE
+    <div className="h-full">
+        <div className="mt-20 w-1/6 hadow-lg p-2 rounded-r-full bg-gradient-to-r from-gray-700 via-gray-900 to-black after:border-amber-600">
+          <div className="text-white text-center text-2xl font-extrabold">
+            <h1 className="p-3">Spline <span>Room</span></h1>
+          </div>
+        </div>
+        <div className="flex h-[30rem] justify-between m-4">
+          <div className="h-auto w-auto flex flex-row">
+            <div className=" flex-col justify-between m-3 space-y-5">
+              <h1 className="font-bold text-2xl">3D Room with Game Controls</h1>
+              <p className="w-64">Learn how to create a 3D scene in Spline with game interactions like moving, jumping, mouse events, and dark mode state.</p>
+              <img className="h-[15rem]" src="https://uploads.codesandbox.io/uploads/user/007827f0-5496-46fa-9529-5def5fbe281a/sh4C-controls.png"/>
+            </div>
+            <div className="w-5/6">
+              <Spline
+                className="spline rounded-full"
+                scene="https://prod.spline.design/5hftbc9FW0gnuhmx/scene.splinecode"
+              />
+            </div>
+          </div>
+        </div>
     </div>
   )
 }
