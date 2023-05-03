8a9cd28fab669e2c629728a5763bce3757a6bd9e
Leo Goubard <leogoubard@macbook-pro-de-leo.home>
2023-05-03 16:33:41 +0200

    :construction: wip

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -4,7 +4,7 @@ import Spline from '@splinetool/react-spline';
 const Home = () => {
   return (
     <div className="h-full text-[#666666] flex justify-center m-20 text-center flex-col">
-        <div className="flex h-[60vh] m-4">
+        <div className="flex h-[50vh] m-4">
           <div className="h-auto w-auto flex flex-row justify-center">
             <div className=" flex-col justify-between m-3 w-2/6">
               <h1 className="font-extrabold text-3xl">Spline Room</h1>
