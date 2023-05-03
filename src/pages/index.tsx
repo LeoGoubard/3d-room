b03994d2e155d746601b00c4f299e19bb1b1b3f9
Leo Goubard <leogoubard@macbook-pro-de-leo.home>
2023-05-03 16:25:09 +0200

    :construction: wip

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,14 +1,13 @@
 import React from 'react';
 import Spline from '@splinetool/react-spline';
 
-
 const Home = () => {
   return (
     <div className="text-[#666666] flex justify-center m-20 text-center flex-col">
-        <div className="flex h-[70vh] m-4">
+        <div className="flex h-[60vh] m-4">
           <div className="h-auto w-auto flex flex-row justify-center">
             <div className=" flex-col justify-between m-3 w-2/6">
-              <h1 className="font-extrabold text-3xl">Spline <span className="">Room</span></h1>
+              <h1 className="font-extrabold text-3xl">Spline Room</h1>
               <h1 className="font-bold text-2xl">3D Room with Game Controls</h1>
               <p className="">Learn how to create a 3D scene in Spline with game interactions like moving, jumping, mouse events, and some interactions in the room.</p>
               <Spline scene="https://prod.spline.design/64vYuxGsTA-XNlX6/scene.splinecode" />
