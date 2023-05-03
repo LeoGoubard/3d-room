73a6809ed7cb9fb635f1130d5e92da5d1101781c
Leo Goubard <leogoubard@macbook-pro-de-leo.home>
2023-05-03 15:43:03 +0200

    :construction: wip

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -13,13 +13,13 @@ const Home = () => {
         </div>
         <div className="flex h-[30rem] justify-between m-4">
           <div className="h-auto w-auto flex flex-row">
-            <div className=" flex-col justify-between m-3 w-3/6">
+            <div className=" flex-col justify-between m-3 w-2/6">
               <h1 className="font-bold text-2xl">3D Room with Game Controls</h1>
               <p className="w-64">Learn how to create a 3D scene in Spline with game interactions like moving, jumping, mouse events, and some interactions in the room.</p>
               <Spline scene="https://prod.spline.design/64vYuxGsTA-XNlX6/scene.splinecode" />
 
             </div>
-            <div className="">
+            <div className="w-4/6">
             <Spline scene="https://prod.spline.design/5hftbc9FW0gnuhmx/scene.splinecode" />
             </div>
           </div>
