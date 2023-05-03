f7ee41e48a223f6230a21127066c7197ecec2c28
Leo Goubard <leogoubard@macbook-pro-de-leo.home>
2023-05-03 16:07:26 +0200

    :construction: wip

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -3,24 +3,18 @@ import Spline from '@splinetool/react-spline';
 
 
 const Home = () => {
-
   return (
-    <div className="h-full text-[#666666] flex justify-center flex-col">
-        <div className="mt-20 w-1/6 hadow-lg p-2">
-          <div className="text-center text-2xl font-extrabold">
-            <h1 className="p-3 font-extrabold text-3xl">Spline <span className="">Room</span></h1>
-          </div>
-        </div>
-        <div className="flex h-[30rem] justify-between m-4">
-          <div className="h-auto w-auto flex flex-row">
+    <div className="h-full text-[#666666] flex justify-center m-20 text-center flex-col">
+        <div className="flex h-[70vh] m-4">
+          <div className="h-auto w-auto flex flex-row justify-center">
             <div className=" flex-col justify-between m-3 w-2/6">
+              <h1 className="font-extrabold text-3xl">Spline <span className="">Room</span></h1>
               <h1 className="font-bold text-2xl">3D Room with Game Controls</h1>
-              <p className="w-64">Learn how to create a 3D scene in Spline with game interactions like moving, jumping, mouse events, and some interactions in the room.</p>
+              <p className="">Learn how to create a 3D scene in Spline with game interactions like moving, jumping, mouse events, and some interactions in the room.</p>
               <Spline scene="https://prod.spline.design/64vYuxGsTA-XNlX6/scene.splinecode" />
-
             </div>
             <div className="w-4/6">
-            <Spline scene="https://prod.spline.design/5hftbc9FW0gnuhmx/scene.splinecode" />
+              <Spline scene="https://prod.spline.design/5hftbc9FW0gnuhmx/scene.splinecode" />
             </div>
           </div>
         </div>
